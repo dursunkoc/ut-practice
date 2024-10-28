05785cd9cd30739f7545e2c4e84de3a4e2ab0ba2
DURSUN KOC <dursun.koc@turkcell.com.tr>
2024-10-28 15:28:19 +0300

    10-running-application

--- src/main/java/io/github/dursunkoc/utpractice/repository/UserRepository.java
+++ src/main/java/io/github/dursunkoc/utpractice/repository/UserRepository.java
@@ -2,15 +2,16 @@ package io.github.dursunkoc.utpractice.repository;
 
 import io.github.dursunkoc.utpractice.domain.User;
 import java.util.Optional;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface UserRepository{
+public interface UserRepository extends JpaRepository<User, String> {
     Optional<User> findByUsername(String username);
 
-    User save(User user);
+    //    User save(User user);
 
-    User update(User user);
+    default User update(User user) {
+        return save(user);
+    }
 }
